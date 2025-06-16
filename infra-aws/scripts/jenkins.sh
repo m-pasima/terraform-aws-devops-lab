8340f41c00e7c73f4323ce29ffe0ac7d78e7edb0
m-pasima <theivykierachronicles@gmail.com>
2025-06-16 17:54:53 +0100

    Fix Jenkins and Tomcat install scripts

--- infra-aws/scripts/jenkins.sh
+++ infra-aws/scripts/jenkins.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-set -e
+#!/bin/bash -xe
+exec &> >(tee /var/log/user-data.log)
 yum update -y
-yum install -y fontconfig java-17-amazon-corretto wget firewalld
-if systemctl is-enabled firewalld >/dev/null 2>&1; then
-    systemctl start firewalld
-    systemctl enable firewalld
-fi
+yum install -y fontconfig java-17-openjdk wget firewalld
+systemctl enable --now firewalld
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
